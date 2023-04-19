7f544339521212697c78cf27e4b2c5c9609612e1
ryank111 <70592671+ryank111@users.noreply.github.com>
2023-04-19 15:30:06 -0400

    iguana climb tree on trigger

---
+++ Assets/AnimateIguana.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateIguana : MonoBehaviour
+{
+    //public BoxCollider boxCollider;
+    public List<Vector3> posPoints;
+    public float speed;
+    Animator iguanaAnimator;
+    int i = 0;
+    bool moveUp = false;
+    bool moveDown = false;
+
+    private void Start()
+    {
+        iguanaAnimator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (i < posPoints.Count && moveUp)
+        {
+            iguanaAnimator.SetBool("MoveFoward", true);
+            iguanaAnimator.SetBool("MoveBack", false);
+            Vector3 partialPoint = Vector3.MoveTowards(transform.localPosition, posPoints[i], speed);
+            transform.localPosition = partialPoint;
+            Debug.Log(partialPoint);
+            if (Vector3.Distance(transform.localPosition, posPoints[i]) < 0.1f) i++;
+        }
+        else if (i > -1 && moveDown)
+        {
+            iguanaAnimator.SetBool("MoveBack", true);
+            iguanaAnimator.SetBool("MoveFoward", false);
+            Vector3 partialPoint = Vector3.MoveTowards(transform.localPosition, posPoints[i], speed);
+            transform.localPosition = partialPoint;
+            Debug.Log(partialPoint);
+            if (Vector3.Distance(transform.localPosition, posPoints[i]) < 0.1f) i--;
+        }
+        else
+        {
+            iguanaAnimator.SetBool("MoveFoward", false);
+            iguanaAnimator.SetBool("MoveBack", false);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            i++;
+            moveUp = true;
+            moveDown = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            i--;
+            moveUp = false;
+            moveDown = true;
+        }
+    }
+}
